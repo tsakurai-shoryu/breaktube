33b21452fda91211ae501496d9f2002ff11c776a
BlosT <tsakurai@aiming-inc.com>
2018-04-06 12:28:11 +0900

    only add and call

---
+++ lib/database.rb
@@ -0,0 +1,29 @@
+require 'sqlite3'
+
+class DataBase
+  def initialize
+    @dbenv = "breaktube-prod.db"
+  end
+
+  def youtube_id_search?(youtube_id)
+    db = SQLite3::Database.new @dbenv
+    db.execute("SELECT youtube_id FROM playlists WHERE youtube_id = \"#{youtube_id}\"").empty?
+  end
+
+  def playlist_id_search(youtube_id)
+    db = SQLite3::Database.new @dbenv
+    db.execute("SELECT id FROM playlists WHERE youtube_id = \"#{youtube_id}\"")
+  end
+
+  def playlists_insert(user_name,youtube_id)
+    c_at = Time.now.to_i
+    db = SQLite3::Database.new @dbenv
+    db.execute("INSERT INTO playlists (user_name, youtube_id, created_at) VALUES (?, ?, ?)",
+               [user_name, youtube_id, c_at])
+  end
+
+  def rand_pick
+    db = SQLite3::Database.new @dbenv
+    db.execute("SELECT youtube_id FROM playlists").flatten.sample
+  end
+end
